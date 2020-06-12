44da9478084805dbca62b60ff3cc70ffe4a87fec
Daniel Mendoza <daniel.mendozapupo@gmail.com>
2020-06-12 22:51:41 +0000

    Updating the setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-with open('README.srt', encoding='UTF-8') as f:
+with open('README.rst', encoding='UTF-8') as f:
     readme = f.read()
 setup( name='hr',
         version ='0.1.0',
-        description = 'Database for human resources',
+        description = 'Commandline user management utility for human resources',
         long_description = readme,
         author = 'Daniel',
         author_email = 'daniel.mendozapupo@gmail.com',
